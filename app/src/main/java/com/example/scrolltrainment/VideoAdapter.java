f394a9a153fa0fcee4e73f0f42f6b1d90950c7f1
ms933 <ms9338504@gmail.com>
2024-01-30 14:25:40 +0530

    Api integration and Video Playback feature added to it. you can also checkout  the profile the person who's post is this.

--- app/src/main/java/com/example/scrolltrainment/VideoAdapter.java
+++ app/src/main/java/com/example/scrolltrainment/VideoAdapter.java
@@ -1,2 +1,69 @@
-package com.example.scrolltrainment;public class VideoAdapter {
+package com.example.scrolltrainment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> {
+
+    Context context;
+    ArrayList<Video> arrayList;
+
+    onItemclickListner onItemclickListner;
+
+    public VideoAdapter(Context context, ArrayList<Video> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.list_of_videoitem,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Glide.with(context).load(arrayList.get(position).getLink()).centerCrop().into(holder.imageView);
+        holder.itemView.setOnClickListener(v -> {
+
+            onItemclickListner.onClick(arrayList.get(position));
+
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            setIsRecyclable(false);
+            imageView= itemView.findViewById(R.id.thumbnail_image);
+
+
+        }
+    }
+
+    public void setOnItemclickListner(VideoAdapter.onItemclickListner onItemclickListner) {
+        this.onItemclickListner = onItemclickListner;
+    }
+
+    public interface onItemclickListner{
+        void onClick(Video video);
+    }
 }
--- app/src/main/java/com/example/scrolltrainment/VideoPlayActivity.java
+++ app/src/main/java/com/example/scrolltrainment/VideoPlayActivity.java
@@ -2,7 +2,11 @@ package com.example.scrolltrainment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class VideoPlayActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class VideoPlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_play);
+
+        String url=getIntent().getStringExtra("url");
+
+        if (url != null) {
+            VideoView videoView = findViewById(R.id.videoview);
+
+            // Use MediaController to enable play/pause controls
+            MediaController mediaController = new MediaController(this);
+            mediaController.setAnchorView(videoView);
+            videoView.setMediaController(mediaController);
+
+            // Set video URI
+            videoView.setVideoURI(Uri.parse(url));
+
+            // Start video asynchronously
+            videoView.setOnPreparedListener(MediaPlayer::start);
+
+            // Finish activity when video completes
+            videoView.setOnCompletionListener(MediaPlayer::release);
+        }
+        else {
+            finish();
+        }
     }
 }
